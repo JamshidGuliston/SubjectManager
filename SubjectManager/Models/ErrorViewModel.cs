3b311e3cd1d26eee3b6775756fc711d539d81288
JamshidGuliston <jamshid_acer@mail.ru>
2023-11-21 06:31:33 +0500

    Добавьте файлы проекта.

---
+++ SubjectManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SubjectManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
